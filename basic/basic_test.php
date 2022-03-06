4b27a075a6439384f13a33434864cd4d9a189d49
gerald-gt <75348576+gerald-gt@users.noreply.github.com>
2022-03-06 10:49:45 +0700

    Update basic_test.php

--- basic/basic_test.php
+++ basic/basic_test.php
@@ -1,1 +1,20 @@
+<?php
+
+/* Author : Gerald Nelson
+   Test   : Befeni Basic Test
+   Version : 1.0
+   */ 
+
+function calculate ( $add = 0 , $apply  = 0 , $multiply = 1 ) {
+
+ $result = ($apply + $add) * $multiply ; 
+
+ return $result ; 
+
+}
+
+// pass relevent inputs for output
+echo calculate($add , $apply , $multiply) ; // input add , apply , multiply values
+
+?>
 
