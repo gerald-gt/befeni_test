052cecf18d939501b7122336df8047dd5882497d
gerald-gt <75348576+gerald-gt@users.noreply.github.com>
2022-03-06 17:40:19 +0700

    Update basic_test.php

--- basic/basic_test.php
+++ basic/basic_test.php
@@ -1,20 +1,77 @@
-<?php
+<!DOCTYPE html>
 
-/* Author : Gerald Nelson
-   Test   : Befeni Basic Test
-   Version : 1.0
-   */ 
+<html>
+	<head>
+		<title>Befeni Test - Basic Test</title>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		 
+		<link href="bootstrap.min.css" rel="stylesheet">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+      <script type="text/javascript">
+      function calculate() {
 
-function calculate ( $add = 0 , $apply  = 0 , $multiply = 1 ) {
+           var s_number = parseInt($("#input_number").val()) ; 
+           var old_number  = parseInt($("#output").text()) ; 
 
- $result = ($apply + $add) * $multiply ; 
+           var total_value = s_number ; 
+           $("#old_number").val(s_number) ;  
+           
+          
+            // select operator
+           var operator = $('#operation :selected').val();
 
- return $result ; 
+           if ( operator == "+" && !!old_number )  {
+            total_value =  s_number +  old_number ; 
+           } 
+           if ( operator == "-" && !!old_number) {
+            total_value =  s_number - old_number ; 
+           } 
+           if ( operator == "*" && !!old_number) {
+            total_value =  s_number *  old_number ; 
+           } 
+           if ( operator == "/" && !!old_number ) {
+            total_value =  s_number /  old_number ; 
+           } 
 
-}
+          $("#input_number").val('') ;
+          $("#output").html(total_value) ;
 
-// pass relevent inputs for output
-echo calculate($add , $apply , $multiply) ; // input add , apply , multiply values
+      }
 
-?>
+      function clear_calc() {
 
+       
+         $("#input_number").val('') ; 
+         $("#old_number").val('') ;
+         $("#output").empty();
+      }
+      </script>
+	</head>
+	<body>
+		
+		<div class="container" style="margin-top: 50px">
+		
+			<h3>calculator - By Gerald </h3>
+		    
+		    <!-- Calculator design by Gerald  -->
+		  
+		        <input name="input_number" id="input_number" type="text" class="form-control" style="width: 150px; display: inline" />
+              <input name="old_number" type="hidden" id="old_number" />
+		        <select name="operation" id="operation">
+		        	<option value="+">+</option>
+		            <option value="-">-</option>
+		            <option value="*">*</option>
+		            <option value="/">/</option>
+		        </select>
+		       
+		        <input name="calc" type="button" value="Calculate" class="btn btn-primary" onclick="calculate();" />
+              <input name="calc_clear" type="button" value="clear" class="btn btn-primary" onclick="clear_calc();" />
+	           
+              <br/><br/>
+              <label id="output"> </label>
+		</div>
+	
+	</body>
+</html>
